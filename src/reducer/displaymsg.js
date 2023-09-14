22e6390e63f3fd3a778d356ad351125043998313
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-09-14 15:22:56 +0530

    1 file modified

--- src/reducer/displaymsg.js
+++ src/reducer/displaymsg.js
@@ -1,22 +1,4 @@
 import React from 'react';
-// import { createSlice } from '@reduxjs/toolkit';
-// const initialState = {
-//   setMessage = '',
-// };
-// export const sendMessage = createSlice({
-//       name:'sendmsg',
-//       initialState,
-//       reducers:{
-//         reset:(state) =>{
-//            state.setMessage = '';
-//         },
-//         enterByUser: (state, action) => {
-//            state.setMessage = action.payload;
-//         },
-//       },
-// });
-// export const { reset, enterByUser } = sendMessage.actions;
-// export default sendMessage.reducer;
 import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   setMessage: '',
