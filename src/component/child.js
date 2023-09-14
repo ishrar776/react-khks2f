5cb6e4e2c7db57d69f73b0ef34f5932ad478d533
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-09-14 15:20:05 +0530

    1 file modified

--- src/component/child.js
+++ src/component/child.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 function Child() {
-  const displayMessage = useSelector((item) => item.sendmsg.sendMessage);
+  const displayMessage = useSelector((item) => item.sendmsg.setMessage);
   return (
     <>
       <main className="App">
